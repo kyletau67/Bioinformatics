96cfe35c2785593b36f596567f184e1e88cdb43f
Kyle Tau <kyletau2@gmail.com>
2023-07-20 18:41:16 -0400

    kmer frequency

--- genome-toolkit/application.py
+++ genome-toolkit/application.py
@@ -3,6 +3,8 @@ from genome_toolkit import genomeToolkit
 gt = genomeToolkit()
 
 seq = "AAAGAAAATTGA"
-kmer = "AAA"
+kmer = "AA"
+k_len = 3
 
 print(gt.count_kmer(seq,kmer))
+print(f'Most frequent k-mer: {gt.find_most_frequent_kmers(seq, k_len)}')
--- genome-toolkit/genome_toolkit.py
+++ genome-toolkit/genome_toolkit.py
@@ -9,3 +9,17 @@ class genomeToolkit:
             if sequence[position:position + len(kmer)] == kmer:
                 kmer_count += 1
         return kmer_count
+    
+    def find_most_frequent_kmers(self, sequence, k_len):
+        """Finds most frequent k-mers of given length in DNA string"""
+        kmer_frequencies = {}
+
+        for i in range(len(sequence) - k_len + 1):
+            kmer = sequence[i:i+k_len]
+            if kmer in kmer_frequencies:
+                kmer_frequencies[kmer] += 1
+            else:
+                kmer_frequencies[kmer] = 1
+        
+        return [kmer for kmer, frequency in kmer_frequencies.items() 
+                if frequency == max(kmer_frequencies.values()) ]
